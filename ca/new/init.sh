db46a3ae15b5c073cf361c5657b3551f74d80cab
mmbros <server.mmbros@yandex.com>
2019-02-16 18:41:54 +0100

    add script folder with ca, intermediate, client and server cert generation scripts

--- ca/new/init.sh
+++ ca/new/init.sh
@@ -28,15 +28,15 @@ PW="--password=pass:secpw"
 # delete '$d' folder
 #rm -rf $d
 # Root CA
-#./gen-cert -t ca-root -d $d/root "$ORG"  "$PW"
+./gen-cert -t ca-root -d $d/root "$ORG"  "$PW"
 # Intermediate CA
-#./gen-cert -t ca-intermediate -d $d/ca1 -c $d/root  --cn="$O CA 1" "$ORG" "$PW"
+./gen-cert -t ca-intermediate -d $d/ca1 -c $d/root  --cn="$O CA 1" "$ORG" "$PW"
 
 # OK
-# ./gen-cert -t server -c $d/ca1 "$ORG" "$PW" \
-# 	--basename='mananno8' --cn='mananno.it' \
-# 	--dns='mananno.it' --dns='mananno.dlinkddns.com' --dns='localhost' \
-# 	--ip 192.168.1.2 --ip 127.0.0.1
+ ./gen-cert -t server -c $d/ca1 "$ORG" "$PW" \
+ 	--basename='mananno' --cn='mananno.it' \
+ 	--dns='mananno.it' --dns='mananno.dlinkddns.com' --dns='localhost' \
+ 	--ip 192.168.1.2 --ip 127.0.0.1
 
 # OK localhost, KO 127.0.0'1
 # ./gen-cert -t server -c $d/ca1 "$ORG" "$PW" \
@@ -44,5 +44,5 @@ PW="--password=pass:secpw"
 # 	--dns='localhost'
 
 
-./gen-cert -t client -c $d/ca1 --basename="mananno.user.5" --cn="Mananno User 5" "$ORG" "$PW"
+#./gen-cert -t client -c $d/ca1 --basename="mananno.user.5" --cn="Mananno User 5" "$ORG" "$PW"
 
