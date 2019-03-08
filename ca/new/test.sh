699a4a55a3696453b21d404aae8cb0570ec190c8
mmbros <server.mmbros@yandex.com>
2019-03-08 22:02:14 +0100

    fix crlnumber file name

--- ca/new/test.sh
+++ ca/new/test.sh
@@ -26,23 +26,29 @@ ORG="--organization=$O"
 PW="--password=pass:secpw"
 
 # delete '$d' folder
-rm -rf $d
-# Root CA
-./gen-cert -t ca-root -d $d/root "$ORG"  "$PW"
-# Intermediate CA
-./gen-cert -t ca-intermediate -d $d/ca1 -c $d/root  --cn="$O CA 1" "$ORG" "$PW"
+# rm -rf $d
 
-## OK
+# Root CA
+#./gen-cert -t ca-root -d $d/root "$ORG"  "$PW"
+
+# Intermediate CA
+# ./gen-cert -t ca-intermediate -d $d/ca1 -c $d/root  --cn="$O CA 1" "$ORG" "$PW"
+
+# server with SAN section
 # ./gen-cert -t server -c $d/ca1 "$ORG" "$PW" \
 # 	--basename='mananno' --cn='mananno.it' \
-# 	--dns='mananno.it' --dns='mananno.dlinkddns.com' --dns='localhost' \
-# 	--ip 192.168.1.2 --ip 127.0.0.1
+#  	--dns='mananno.it' --dns='mananno.dlinkddns.com' --dns='localhost' \
+#  	--ip 192.168.1.2 --ip 127.0.0.1
 
-# OK localhost, KO 127.0.0'1
+# server without SAN section
 # ./gen-cert -t server -c $d/ca1 "$ORG" "$PW" \
 # 	--basename='mananno9' --cn='mananno.it' \
 # 	--dns='localhost'
 
 
-#./gen-cert -t client -c $d/ca1 --basename="mananno.user.5" --cn="Mananno User 5" "$ORG" "$PW"
+# client
+./gen-cert -t client -c $d/ca1 "$ORG" "$PW" \
+	--basename="admin_mmbros.it"
+	--cn="Administrator"
+	--email="admin@mmbros.it"
 
