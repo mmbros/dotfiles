c13b1613b64eb924ca2f5476a250bd469d7430d7
mmbros <server.mmbros@yandex.com>
2019-03-29 23:22:31 +0100

    save old work (hope not an error)

--- ca/new/test.sh
+++ ca/new/test.sh
@@ -19,23 +19,23 @@ set -e
 
 O="MMbros"
 # common directory prefix
-d="$HOME/ca-demo"
+d="/media/crypt/ca"
 
 
 ORG="--organization=$O"
-PW="--password=pass:secpw"
+#PW="--password=pass:secpw"
 
 # delete '$d' folder
 # rm -rf $d
 
 # Root CA
-#./gen-cert -t ca-root -d $d/root "$ORG"  "$PW"
+# ./gen-cert -t ca-root -d $d/root "$ORG"  "$PW"
 
 # Intermediate CA
-# ./gen-cert -t ca-intermediate -d $d/ca1 -c $d/root  --cn="$O CA 1" "$ORG" "$PW"
+# ./gen-cert -t ca-intermediate -d $d/ca1 -c $d/root  --cn="$O CA 1" "$ORG"
 
 # server with SAN section
-# ./gen-cert -t server -c $d/ca1 "$ORG" "$PW" \
+# ./gen-cert -t server -c $d/ca1 "$ORG"  \
 # 	--basename='mananno' --cn='mananno.it' \
 #  	--dns='mananno.it' --dns='mananno.dlinkddns.com' --dns='localhost' \
 #  	--ip 192.168.1.2 --ip 127.0.0.1
@@ -47,8 +47,6 @@ PW="--password=pass:secpw"
 
 
 # client
-./gen-cert -t client -c $d/ca1 "$ORG" "$PW" \
-	--basename="admin_mmbros.it"
-	--cn="Administrator"
-	--email="admin@mmbros.it"
+./gen-cert -t client -c $d/ca1 "$ORG"  \
+	--cn="xub.pcfamily@mananno.it"
 
